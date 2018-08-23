1621b5bdb25eba8bdd44a2e8bb254819040e33e7
Amal Raj K S <amalrajks@gmail.com>
2018-08-23 17:52:36 +0530

    basic structure

--- bellerophon.py
+++ bellerophon.py
@@ -1,2 +1,25 @@
-# sample
-print('Hello World')
+"""docstring placeholder"""
+import os
+
+path = os.path.join(os.getcwd(), r'tests\garbbage')
+dirs = []
+files = []
+excluded_strings = ['amal', 'hurry']
+for e in os.scandir(path):
+    if e.is_file():
+        filename = e.name
+        file_info = []
+        #file_info = [(f, e.name, filename.replace(f, ''), 'RENAMED') for f in excluded_strings if f in e.name]
+        for unwanted in excluded_strings:
+            if unwanted in filename:
+                new_filename = filename.replace(unwanted, '')
+                file_info.append((filename, new_filename, 'RENAMED', unwanted))
+                filename = new_filename
+        if file_info.__len__() > 0:
+            files.append(file_info)
+        else:
+            files.append((filename, filename, 'NO_CHANGE', None))
+    elif e.is_dir():
+        dirs.append(e.name)
+print(files)
+#print(dirs)
--- tests/bellerophon_tests.py
+++ tests/bellerophon_tests.py
@@ -1,1 +1,22 @@
-#sample
+"""placeholder docstring"""
+
+import os
+
+path = os.getcwd()
+print(path)
+
+"""
+if not os.path.exists(os.path.join(path, r'howareyou_amal')):
+    os.makedirs(os.path.join(path, r'howareyou_amal'))
+if not os.path.exists(os.path.join(path, r'test_folder1')):
+    os.makedirs(os.path.join(path, r'test_folder1'))
+
+with open(os.path.join(path, 'amalgoodhurry.txt'), 'w'):
+    pass
+with open(os.path.join(path, 'idontthinkamal.txt'), 'w'):
+    pass
+with open(os.path.join(path, 'rem.amal.txt'), 'w'):
+    pass
+with open(os.path.join(path, 'test.txt'), 'w'):
+    pass
+"""
