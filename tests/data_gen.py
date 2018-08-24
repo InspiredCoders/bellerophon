6b747699cd658d0eb91ed1ca86f814807ff7f930
linson-93 <webclassrooms@gmail.com>
2018-08-24 19:44:06 +0530

    Fixed pep8 errors

--- tests/data_gen.py
+++ tests/data_gen.py
@@ -9,7 +9,6 @@ import random
 import time
 
 
-
 def dir_gen(directory_name):
     """
     Function to create files.
@@ -32,17 +31,19 @@ def file_gen(file_name):
     file_hanlder.close()
 
 
+def millis_time():
+    return str(int(round(time.time())))
+
 
 def main():
-    parent_directory = os.path.join('tests','garbbage')
-    epochs           = 10  # Increase the epochs to create more files and dirs
-    is_directory     = [True, False]
+    parent_directory = os.path.join('tests', 'garbbage')
+    epochs = 10  # Increase the epochs to create more files and dirs
+    is_directory = [True, False]
     name_list = ['amal', 'hurry', 'test', 'random', 'orange', 'goat', 'normal']
     extension = ['txt', 'mp3', 'cpp', 'hpp', 'mkv', 'wav', 'mp4', 'lst', 'cap']
-    millis_time = lambda: str(int(round(time.time())))
-    dir_count  = 0
+    dir_count = 0
     file_count = 0
-    
+
     dir_gen(parent_directory)
 
     for _ in range(epochs):
@@ -51,7 +52,7 @@ def main():
             dir_gen(dir_name)
             dir_count += 1
         else:
-            extn     = random.choice(extension)
+            extn = random.choice(extension)
             name_part = random.choice(name_list)
             full_name = name_part + '_' + millis_time() + '.' + extn
             file_name = os.path.join(parent_directory, full_name)
@@ -59,7 +60,7 @@ def main():
             file_count += 1
 
     print('Created {} files and {} directories in {}'
-        .format(file_count, dir_count, parent_directory))
+          .format(file_count, dir_count, parent_directory))
 
 if __name__ == '__main__':
     main()
