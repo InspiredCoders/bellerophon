267d82daac7206bae581052ac4befddeb0c488d4
linson-93 <webclassrooms@gmail.com>
2018-08-27 19:44:50 +0530

    Added config and documented code

--- bellerophon/config_manager.py
+++ bellerophon/config_manager.py
@@ -1,9 +1,19 @@
+"""
+config_manager.py
+
+File containing the functionality to read config file and to provide
+values of specified keys
+"""
 import json
 import os
 
 
 class ConfigManager:
+    """
+    ConfigManager class
 
+    Class which handles the configuration realated functionalities.
+    """
     current_dir = os.path.dirname(os.path.relpath(__file__))
 
     def __init__(self):
@@ -11,6 +21,10 @@ class ConfigManager:
         self.data = self.read_config()
 
     def read_config(self):
+        """
+        Function to read configuration from json file and store it in
+        the class member `data`
+        """
         try:
             with open(self.filepath, 'r') as json_file:
                 data = json.load(json_file)
@@ -21,11 +35,16 @@ class ConfigManager:
             raise Exception('Configuration file is empty')
 
     def get_config(self, key):
+        """
+        Function to retrieve a sepecific configuration value by providing
+        the correct key.
+
+        :param string key: The key for the configuration which is to
+        be retrieved
+
+        :returns value object: The value for the key specified in arguments
+        """
         try:
             return self.data[key]
         except KeyError:
             raise KeyError('The provided key is not there in config file')
-
-
-if __name__ == '__main__':
-    config = ConfigManager()
