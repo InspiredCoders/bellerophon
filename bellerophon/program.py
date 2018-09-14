1df9bfad4d41a5cc51323718af680d8c7e48ce77
Amal Raj K S <amalrajks@gmail.com>
2018-09-14 16:30:34 +0530

    Find and Replace and a change in the use of Bellerophon class

--- bellerophon/program.py
+++ bellerophon/program.py
@@ -16,13 +16,10 @@ def main():
     blacklist_file_location = os.path.join(
         ConfigManager.current_dir, blacklist_file_location
         )
-    blacklisted_strings = [
-        line.replace('\n', '') for line in open(blacklist_file_location, "r")
-        ]
     target_location = input(
         "Provide target directory location(fully qualified location):"
         )
-    bellerophone = Bellerophone(blacklisted_strings)
+    bellerophone = Bellerophone(blacklist_file_location)
     print("Renaming is in progress...")
     bellerophone.crawl_and_rename(target_location, True)
     if bellerophone.log == []:
@@ -39,5 +36,37 @@ def main():
             )
     print("Execution completed.")
 
+
+def find_replace():
+    """
+    Find and Replace
+    """
+    program_config = ConfigManager()
+    db_location = program_config.get_config("database_location")
+
+    target_location = input(
+        "Provide target directory location(fully qualified location):"
+        )
+    bellerophone = Bellerophone("")
+    old_string = input("FIND:")
+    if old_string:
+        new_string = input("REPLACE:")
+        if new_string:
+            bellerophone.crawl_and_find_and_replace(target_location, True, old_string, new_string)
+        if bellerophone.log == []:
+            print("No objects to rename!")
+        else:
+            print("Renaming is completed and saving tracks...")
+        with DatabaseContext(db_location) as db:
+            db.create_processing_history_table()
+        with DatabaseContext(db_location) as db:
+            for child in bellerophone.log:
+                db.insert_into_processing_history_table(
+                    child[0], child[1], child[2],
+                    child[3], child[4], child[5], child[6], child[7]
+                )
+    print("Execution completed.")
+
+
 if __name__ == '__main__':
-    main()
+    find_replace()
